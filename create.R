fd9d24fd3bec476e23b86ac8f69cb663e1c23657
Emily Mitchell <Emily.Mitchell@ahrq.hhs.gov>
2020-08-07 20:12:44 -0400

    Updating meps-r-pkg with functions to download ASCII (.dat) PUFs

--- create.R
+++ create.R
@@ -2,7 +2,11 @@ library('devtools')
 library(roxygen2)
 library(dplyr)
 library(tidyr)
+library(testthat)
+library(knitr)
 
+library(devtools)
+has_devel()
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # setwd("C:/Users/emily.mitchell/Desktop/Programming/GitHub/meps_r_pkg")
@@ -19,20 +23,20 @@ devtools::document()
 # Add collapsed condition codes data to package -------------------------------
 setwd("..")
 
-cnd_codes <- read.csv("../hhs_ahrq/MEPS/Quick_Reference_Guides/meps_condition_codes.csv")
+# cnd_codes <- read.csv("../hhs_ahrq/MEPS/Quick_Reference_Guides/meps_condition_codes.csv")
+# 
+# condition_codes <- cnd_codes %>% 
+#   separate_rows(CCS.Codes,sep=",") %>%
+#   separate(CCS.Codes,sep="-",c("min","max"),fill='right') %>%
+#   mutate(max = ifelse(is.na(max),min,max)) %>% 
+#   rowwise %>%
+#   mutate(CCS_Codes = paste0(min:max,collapse=",")) %>% 
+#   ungroup %>%
+#   separate_rows(CCS_Codes,sep=",") %>%
+#   mutate(CCS_Codes = as.numeric(as.character(CCS_Codes))) %>%
+#   select(Condition, CCS_Codes)
 
-condition_codes <- cnd_codes %>% 
-  separate_rows(CCS.Codes,sep=",") %>%
-  separate(CCS.Codes,sep="-",c("min","max"),fill='right') %>%
-  mutate(max = ifelse(is.na(max),min,max)) %>% 
-  rowwise %>%
-  mutate(CCS_Codes = paste0(min:max,collapse=",")) %>% 
-  ungroup %>%
-  separate_rows(CCS_Codes,sep=",") %>%
-  mutate(CCS_Codes = as.numeric(as.character(CCS_Codes))) %>%
-  select(Condition, CCS_Codes)
-
-usethis::use_data(condition_codes, overwrite = F)
+# usethis::use_data(condition_codes, overwrite = F)
 
 # Add TC1 codes data to packages ----------------------------------------------
 
